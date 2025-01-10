88926685fb1c569aa150912783c28d6b16d435f2
ShafinKhan-Pathan <kshafin575@gmail.com>
2025-01-10 00:55:55 -0600

    Update Login and Build Sign up page

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,26 +1,93 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Login.css";
 import login_logo from "../../assets/logo.png";
 import login_background from "../../assets/login_backgroundImage.jpg";
 const Login = () => {
+  const [signState, setSignState] = useState("Sign In");
+  const [passwordVisible, setPasswordVisible] = useState(false);
+  const togglePasswordVisibility = () => {
+    setPasswordVisible(!passwordVisible);
+  };
   return (
     <div className="login">
       <div class="bgimage"></div>
       <img src={login_logo} alt="login_bgImage" />
       <div className="login_box">
         <div className="login_form">
-        <h1>Sign In</h1>
+          <h1>{signState}</h1>
           <form className="login__form--fields">
-            <input type="text" placeholder="Email or mobile number" className="input_design"/>
-            <input type="password" placeholder="Password" className="input_design"/>
+            {signState === "Sign Up" ? (
+              <input
+                type="text"
+                placeholder="Your FullName"
+                className="input_design"
+              />
+            ) : (
+              <></>
+            )}
+
+            <input
+              type="text"
+              placeholder="Email or mobile number"
+              className="input_design"
+            />
+            <div className="password_container">
+            <input
+              type={passwordVisible ? "text" : "password"}
+              id="password"
+              placeholder="Password"
+              className="input_design"
+            />
+            <button
+              type="button"
+              id="togglePassword"
+              class="toggle_password"
+              onClick={togglePasswordVisibility}
+            >
+              {passwordVisible ? "👁️‍🗨️" : "👁️"}
+            </button>
+            </div>
+            <div className="remember">
+              <input type="checkbox" />
+              <label htmlFor="">Remember Me</label>
+            </div>
+            {signState === "Sign Up" ? (
+              <button className="form_btn">Sign Up</button>
+            ) : (
+              <button className="form_btn">Sign In</button>
+            )}
           </form>
-          <button className="form_btn">Sign In</button>
           <h3>OR</h3>
-          <button className="form_btn-light">Use Sign-In Code</button>
-          <p className="form_p">
-            New to Netflix?<a href=""><span className="span__modifier">Sign Up now.</span></a>
+          {signState === "Sign In" ? (
+            <p className="form_p">
+              New to Netflix?
+              <span
+                className="span__modifier"
+                onClick={() => {
+                  setSignState("Sign Up");
+                }}
+              >
+                Sign Up now.
+              </span>
+            </p>
+          ) : (
+            <p className="form_p">
+              Already Have an Account?
+              <span
+                className="span__modifier"
+                onClick={() => {
+                  setSignState("Sign In");
+                }}
+              >
+                Sign In now.
+              </span>
+            </p>
+          )}
+
+          <p className="para_notice">
+            This page is protected by Google reCAPTCHA to ensure you're not a
+            bot.{" "}
           </p>
-          <p className="para_notice">This page is protected by Google reCAPTCHA to ensure you're not a bot. </p>
         </div>
       </div>
     </div>
