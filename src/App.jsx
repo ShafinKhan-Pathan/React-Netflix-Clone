53ad802f96ced4055e6e1394d115cbf2c26f3af8
ShafinKhan-Pathan <kshafin575@gmail.com>
2025-01-12 03:10:28 -0600

    Fix Vercel Issues

--- src/App.jsx
+++ src/App.jsx
@@ -5,24 +5,22 @@ import { Route, Routes, useNavigate } from "react-router-dom";
 import Player from "./pages/Player/Player";
 import { onAuthStateChanged } from "firebase/auth";
 import { auth } from "./firebase";
-import { ToastContainer, toast } from 'react-toastify';
+import { ToastContainer, toast } from "react-toastify";
 
 const App = () => {
   const navigate = useNavigate();
   useEffect(() => {
     onAuthStateChanged(auth, async (user) => {
       if (user) {
-        console.log("Logged In");
-        navigate('/')
+        navigate("/");
       } else {
-        console.log("Logged Out");
-        navigate('/login')
+        navigate("/login");
       }
     });
   }, []);
   return (
     <div>
-       <ToastContainer theme="dark"/>
+      <ToastContainer theme="dark" />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
